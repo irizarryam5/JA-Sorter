3f79e3c61c5e5b3ecc3a5e5560f6693f4f9a5589
Alex Irizarry <irizarryam5@gmail.com>
2019-02-26 18:03:42 -0500

    extremely rough and uncommented memory optimized version

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,8 +5,8 @@
 #include <string>
 #include <vector>
 #include <fstream>
-#include <iomanip> //for std::setw
-#include <stdio.h> //for printf
+//#include <iomanip> //for std::setw
+//#include <stdio.h> //for printf
 #include <algorithm>
 #include <random>
 
@@ -42,7 +42,7 @@ static std::vector<std::vector<std::string>> generateListOfLists(const std::vect
 static std::vector<listElement> convertFullListToVector(const std::string& full_list_path);
 static std::vector<clientRequest> convertRequestsToVector(const std::string& req_list_path);
 static std::vector<std::vector<int>> listSplitter(const clientRequest& client_req, const std::vector<listElement>& full_list);
-static std::vector<listElement> generateExportableList(const clientRequest& req_list, std::vector<listElement>& full_list, const std::vector<std::string>& hash);
+static void generateExportableList(const std::vector<clientRequest>& client_req, std::vector<listElement>& full_list, const std::vector<std::vector<std::string>>& hash);
 static zipBounds getStateZipBounds(const int& zip);
 
 int main() {
@@ -64,19 +64,21 @@ int main() {
 
 	//debug mode breakpoint
 	int jeff = 0;
+
+	generateExportableList(clientRequests, fullList, listOfHashes);
 	  
-	for (size_t i = 0; i<clientRequests.size(); i++) {
-		std::ofstream outfile(("C:\\Users\\bluei\\Desktop\\"+clientRequests[i].organizationName+"_JA_complete.csv"));
-		std::vector<listElement> exporter = generateExportableList(clientRequests[i], fullList, listOfHashes[i]);
-	  
-	outfile << "id,firstName,lastName,email,timesSwapped,zip,hash" << std::endl;
-	  
-	for (listElement& el : exporter)
-		outfile << el.id << ',' << el.firstName << ',' << el.lastName << ',' << el.email << ',' << el.timesSwapped << ',' << el.zip << ',' << el.hash << std::endl;
-	  
-		outfile.close();
-	}
-	  
+	//**************for (size_t i = 0; i<clientRequests.size(); i++) {
+	//**************	std::ofstream outfile(("C:\\Users\\bluei\\Desktop\\"+clientRequests[i].organizationName+"_JA_complete.csv"));
+	//**************	std::vector<listElement> exporter = generateExportableList(clientRequests[i], fullList, listOfHashes[i]);
+	//**************  
+	//**************outfile << "id,firstName,lastName,email,timesSwapped,zip,hash" << std::endl;
+	//**************  
+	//**************for (listElement& el : exporter)
+	//**************	outfile << el.id << ',' << el.firstName << ',' << el.lastName << ',' << el.email << ',' << el.timesSwapped << ',' << el.zip << ',' << el.hash << std::endl;
+	//**************  
+	//**************	outfile.close();
+	//**************}
+	//**************  
 	std::ofstream outfile(("C:\\Users\\bluei\\Desktop\\full_list_out.csv"));
 	outfile << "id,firstName,lastName,email,timesSwapped,zip,hash" << std::endl;
 	  
@@ -84,9 +86,9 @@ int main() {
 		outfile << el.id << ',' << el.firstName << ',' << el.lastName << ',' << el.email << ',' << el.timesSwapped << ',' << el.zip << ',' << el.hash << std::endl;
 	  
 	outfile.close();
-	//remember to use this syntax when iterating through vectors
-	//for (std::string& s : LIST_PATHS)
-	//	std::cout << s << std::endl;
+	//**************//remember to use this syntax when iterating through vectors
+	//**************//for (std::string& s : LIST_PATHS)
+	//**************//	std::cout << s << std::endl;
 
 	std::cout << "Press any key to exit the terminal fam." <<std::endl;
 	std::cin.get();
@@ -527,69 +529,164 @@ static std::vector<std::vector<int>> listSplitter(const clientRequest& client_re
 	return listToReturn;
 }
 
+struct abacus {
+	size_t countTotal = 0;
+	size_t count0 = 0;
+	size_t count1 = 0;
+	size_t count2 = 0;
+};
 
-static std::vector<listElement> generateExportableList(const clientRequest& client_req, std::vector<listElement>& full_list, const std::vector<std::string>& hash) {
-	std::vector<listElement> listToReturn;
-	listToReturn.reserve(15000);
-	std::vector<listElement> listToShuffle = full_list;
-	auto rng = std::default_random_engine{};
+static void generateExportableList(const std::vector<clientRequest>& client_req_list, std::vector<listElement>& full_list, const std::vector<std::vector<std::string>>& hash) {
+	std::ofstream outfile;
+	std::vector<abacus> counterArr(client_req_list.size());
+	std::vector<std::vector<std::vector<int>>> preferredListIDs;
+	preferredListIDs.reserve(58000*client_req_list.size());
+	for (size_t i = 0; i < client_req_list.size(); i++) { preferredListIDs.push_back(listSplitter(client_req_list[i], full_list)); }
 	
+	//ALSO in the if statements make checks to see if the counterArr[i].countX doesn't
+	//Exceed the length of preferredListIDs[i][X]
 
-	std::shuffle(std::begin(listToShuffle), std::end(listToShuffle), rng);
-	
-	//consider checking ZIP code data type first and second memeber according to ZIP code schema
-	if (client_req.preferredZipCode != 0) {
-		std::vector<std::vector<int>> splitLists = listSplitter(client_req, full_list);
+	size_t iterator = 0;
+	bool condition = true;
 
-		for (size_t i = 0; i < splitLists[0].size(); i++) {
-			if (listToReturn.size() < static_cast<size_t>(client_req.numOfNames)) {
-				if (!(std::find(hash.begin(), hash.end(), full_list[splitLists[0][i]].hash) != hash.end()) && full_list[splitLists[0][i]].timesSwapped < 3) {
-					full_list[splitLists[0][i]].timesSwapped++;
-					listToReturn.push_back(full_list[splitLists[0][i]]);
-				}
-			} else {
-				break;
-			}
-		}
-
-		for (size_t i = 0; i < splitLists[1].size(); i++) {
-			if (listToReturn.size() < static_cast<size_t>(client_req.numOfNames)) {
-				if (!(std::find(hash.begin(), hash.end(), full_list[splitLists[1][i]].hash) != hash.end()) && full_list[splitLists[1][i]].timesSwapped < 3) {
-					full_list[splitLists[1][i]].timesSwapped++;
-					listToReturn.push_back(full_list[splitLists[1][i]]);
-				}
-			}
-			else {
-				break;
-			}
-		}
-
-		for (size_t i = 0; i < splitLists[2].size(); i++) {
-			if (listToReturn.size() < static_cast<size_t>(client_req.numOfNames)) {
-				if (!(std::find(hash.begin(), hash.end(), full_list[splitLists[2][i]].hash) != hash.end()) && full_list[splitLists[2][i]].timesSwapped < 3) {
-					full_list[splitLists[2][i]].timesSwapped++;
-					listToReturn.push_back(full_list[splitLists[2][i]]);
-				}
-			}
-			else {
-				break;
-			}
-		}
-	} else {
-		for (size_t i = 0; i < listToShuffle.size(); i++) {
-			if (listToReturn.size() < static_cast<size_t>(client_req.numOfNames)) {
-				if (!(std::find(hash.begin(), hash.end(), listToShuffle[i].hash) != hash.end()) && full_list[listToShuffle[i].id].timesSwapped < 3) {
-					full_list[listToShuffle[i].id].timesSwapped++;
-					listToReturn.push_back(listToShuffle[i]);
-				}
-			}
-			else {
-				break;
-			}
-		}
+	for (size_t i = 0; i < client_req_list.size(); i++) {
+		outfile.open(("C:\\Users\\bluei\\Desktop\\output\\" + client_req_list[i].organizationName + "_JA_complete.csv"), std::ios_base::app);
+		outfile << "id,firstName,lastName,email,timesSwapped,zip,hash" << std::endl;
+		outfile.close();
 	}
 
-	return listToReturn;
+	while (condition) {
+		for (size_t i = 0; i < client_req_list.size(); i++) {
+			if (counterArr[i].countTotal >= client_req_list[i].numOfNames) { continue; } //consider creating a bool array where values are set to true only if countArr[i].countX >= preferredListIDs[i][x] in addition to the current if condition
+
+			if (preferredListIDs[i][0].size() > counterArr[i].count0) { /*************/
+				if (!(std::find(hash[i].begin(), hash[i].end(), full_list[preferredListIDs[i][0][counterArr[i].count0]].hash) != hash[i].end()) && full_list[preferredListIDs[i][0][counterArr[i].count0]].timesSwapped < 3) {
+					outfile.open(("C:\\Users\\bluei\\Desktop\\output\\" + client_req_list[i].organizationName + "_JA_complete.csv"), std::ios_base::app);
+					outfile << full_list[preferredListIDs[i][0][counterArr[i].count0]].id << ','
+						<< full_list[preferredListIDs[i][0][counterArr[i].count0]].firstName << ','
+						<< full_list[preferredListIDs[i][0][counterArr[i].count0]].lastName << ','
+						<< full_list[preferredListIDs[i][0][counterArr[i].count0]].email << ','
+						<< full_list[preferredListIDs[i][0][counterArr[i].count0]].timesSwapped << ','
+						<< full_list[preferredListIDs[i][0][counterArr[i].count0]].zip << ','
+						<< full_list[preferredListIDs[i][0][counterArr[i].count0]].hash << std::endl;
+					outfile.close();
+					full_list[preferredListIDs[i][0][counterArr[i].count0]].timesSwapped++;
+					counterArr[i].countTotal++;
+
+					//std::cout << full_list[preferredListIDs[i][0][counterArr[i].count0]].id << '\t' << full_list[preferredListIDs[i][0][counterArr[i].count0]].firstName << '\t' << full_list[preferredListIDs[i][0][counterArr[i].count0]].lastName << '\t' << full_list[preferredListIDs[i][0][counterArr[i].count0]].email << '\t' << full_list[preferredListIDs[i][0][counterArr[i].count0]].zip << '\t' << full_list[preferredListIDs[i][0][counterArr[i].count0]].hash << '\t' << full_list[preferredListIDs[i][0][counterArr[i].count0]].timesSwapped << std::endl;
+				}
+
+				counterArr[i].count0++;
+
+			} else if (preferredListIDs[i][1].size() > counterArr[i].count1) { /**************/
+				if (!(std::find(hash[i].begin(), hash[i].end(), full_list[preferredListIDs[i][1][counterArr[i].count1]].hash) != hash[i].end()) && full_list[preferredListIDs[i][1][counterArr[i].count1]].timesSwapped < 3) {
+					outfile.open(("C:\\Users\\bluei\\Desktop\\output\\" + client_req_list[i].organizationName + "_JA_complete.csv"), std::ios_base::app);
+					outfile << full_list[preferredListIDs[i][1][counterArr[i].count1]].id << ','
+						<< full_list[preferredListIDs[i][1][counterArr[i].count1]].firstName << ','
+						<< full_list[preferredListIDs[i][1][counterArr[i].count1]].lastName << ','
+						<< full_list[preferredListIDs[i][1][counterArr[i].count1]].email << ','
+						<< full_list[preferredListIDs[i][1][counterArr[i].count1]].timesSwapped << ','
+						<< full_list[preferredListIDs[i][1][counterArr[i].count1]].zip << ','
+						<< full_list[preferredListIDs[i][1][counterArr[i].count1]].hash << std::endl;
+					outfile.close();
+					full_list[preferredListIDs[i][1][counterArr[i].count1]].timesSwapped++;
+					counterArr[i].countTotal++;
+
+					//std::cout << full_list[preferredListIDs[i][1][counterArr[i].count1]].id << '\t' << full_list[preferredListIDs[i][1][counterArr[i].count1]].firstName << '\t' << full_list[preferredListIDs[i][1][counterArr[i].count1]].lastName << '\t' << full_list[preferredListIDs[i][1][counterArr[1].count1]].email << '\t' << full_list[preferredListIDs[i][1][counterArr[i].count1]].zip << '\t' << full_list[preferredListIDs[i][1][counterArr[i].count1]].hash << '\t' << full_list[preferredListIDs[i][1][counterArr[i].count1]].timesSwapped << std::endl;
+				}
+
+				counterArr[i].count1++;
+
+			} else if (preferredListIDs[i][2].size() > counterArr[i].count2) { /***************/
+				if (!(std::find(hash[i].begin(), hash[i].end(), full_list[preferredListIDs[i][2][counterArr[i].count2]].hash) != hash[i].end()) && full_list[preferredListIDs[i][2][counterArr[i].count2]].timesSwapped < 3) {
+					outfile.open(("C:\\Users\\bluei\\Desktop\\output\\" + client_req_list[i].organizationName + "_JA_complete.csv"), std::ios_base::app);
+					outfile << full_list[preferredListIDs[i][2][counterArr[i].count2]].id << ','
+						<< full_list[preferredListIDs[i][2][counterArr[i].count2]].firstName << ','
+						<< full_list[preferredListIDs[i][2][counterArr[i].count2]].lastName << ','
+						<< full_list[preferredListIDs[i][2][counterArr[i].count2]].email << ','
+						<< full_list[preferredListIDs[i][2][counterArr[i].count2]].timesSwapped << ','
+						<< full_list[preferredListIDs[i][2][counterArr[i].count2]].zip << ','
+						<< full_list[preferredListIDs[i][2][counterArr[i].count2]].hash << std::endl;
+					outfile.close();
+					full_list[preferredListIDs[i][2][counterArr[i].count2]].timesSwapped++;
+					counterArr[i].countTotal++;
+
+					//std::cout << full_list[preferredListIDs[i][2][counterArr[i].count2]].id << '\t' << full_list[preferredListIDs[i][2][counterArr[i].count2]].firstName << '\t' << full_list[preferredListIDs[i][2][counterArr[i].count2]].lastName << '\t' << full_list[preferredListIDs[i][2][counterArr[i].count2]].email << '\t' << full_list[preferredListIDs[i][2][counterArr[i].count2]].zip << '\t' << full_list[preferredListIDs[i][2][counterArr[i].count2]].hash << '\t' << full_list[preferredListIDs[i][2][counterArr[i].count2]].timesSwapped << std::endl;
+				}
+
+				counterArr[i].count2++;
+			}
+		}
+	
+		size_t points = 0;
+
+		for (size_t i = 0; i < client_req_list.size(); i++) { 
+			if (counterArr[i].countTotal >= client_req_list[i].numOfNames ) { 
+				points++; 
+			} else if (counterArr[i].count0 >= preferredListIDs[i][0].size() 
+					&& counterArr[i].count1 >= preferredListIDs[i][1].size() 
+					&& counterArr[i].count2 >= preferredListIDs[i][2].size()){
+					
+				points++;
+			}
+		}
+
+		if (points == client_req_list.size()) { condition = false; }
+
+		iterator++;
+	}
+	
+	//consider checking ZIP code data type first and second memeber according to ZIP code schema
+	//*********if (client_req.preferredZipCode != 0) {
+	//*********	std::vector<std::vector<int>> splitLists = listSplitter(client_req, full_list);
+	//*********
+	//*********	for (size_t i = 0; i < splitLists[0].size(); i++) {
+	//*********		if (listToReturn.size() < static_cast<size_t>(client_req.numOfNames)) {
+	//*********			if (!(std::find(hash.begin(), hash.end(), full_list[splitLists[0][i]].hash) != hash.end()) && full_list[splitLists[0][i]].timesSwapped < 3) {
+	//*********				full_list[splitLists[0][i]].timesSwapped++;
+	//*********				listToReturn.push_back(full_list[splitLists[0][i]]);
+	//*********			}
+	//*********		} else {
+	//*********			break;
+	//*********		}
+	//*********	}
+	//*********
+	//*********	for (size_t i = 0; i < splitLists[1].size(); i++) {
+	//*********		if (listToReturn.size() < static_cast<size_t>(client_req.numOfNames)) {
+	//*********			if (!(std::find(hash.begin(), hash.end(), full_list[splitLists[1][i]].hash) != hash.end()) && full_list[splitLists[1][i]].timesSwapped < 3) {
+	//*********				full_list[splitLists[1][i]].timesSwapped++;
+	//*********				listToReturn.push_back(full_list[splitLists[1][i]]);
+	//*********			}
+	//*********		}
+	//*********		else {
+	//*********			break;
+	//*********		}
+	//*********	}
+	//*********
+	//*********	for (size_t i = 0; i < splitLists[2].size(); i++) {
+	//*********		if (listToReturn.size() < static_cast<size_t>(client_req.numOfNames)) {
+	//*********			if (!(std::find(hash.begin(), hash.end(), full_list[splitLists[2][i]].hash) != hash.end()) && full_list[splitLists[2][i]].timesSwapped < 3) {
+	//*********				full_list[splitLists[2][i]].timesSwapped++;
+	//*********				listToReturn.push_back(full_list[splitLists[2][i]]);
+	//*********			}
+	//*********		}
+	//*********		else {
+	//*********			break;
+	//*********		}
+	//*********	}
+	//*********} else {
+	//*********	for (size_t i = 0; i < listToShuffle.size(); i++) {
+	//*********		if (listToReturn.size() < static_cast<size_t>(client_req.numOfNames)) {
+	//*********			if (!(std::find(hash.begin(), hash.end(), listToShuffle[i].hash) != hash.end()) && full_list[listToShuffle[i].id].timesSwapped < 3) {
+	//*********				full_list[listToShuffle[i].id].timesSwapped++;
+	//*********				listToReturn.push_back(listToShuffle[i]);
+	//*********			}
+	//*********		}
+	//*********		else {
+	//*********			break;
+	//*********		}
+	//*********	}
+	//*********}
 }
 
 static zipBounds getStateZipBounds(const int& zip) {
